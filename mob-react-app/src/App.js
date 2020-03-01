8187754daae914a3dcdd3610ca6b0960ee272c87
Matthew Inman Cochrane <matthewinmancochrane@simple.com>
2020-02-29 19:24:53 -0800

    Reformat to width of 80

--- mob-react-app/src/App.js
+++ mob-react-app/src/App.js
@@ -18,18 +18,25 @@ function App() {
         Mob React App
       </header>
       <div className="counter-tally-container">
-        <span className="hidden-label" id="counter-tally-label">counter tally</span>
-        <span className="tally" aria-labelledby="counter-tally-label">{clickCount}</span>
+        <span className="hidden-label"
+              id="counter-tally-label">counter tally</span>
+        <span className="tally"
+              aria-labelledby="counter-tally-label">{clickCount}</span>
       </div>
       <div className="counter-button-container">
-        <span className="hidden-label" id="counter-button-label">counter button</span>
-        <button className="counter-button" aria-labelledby="counter-button-label" onClick={incrementTally}>Click to
+        <span className="hidden-label"
+              id="counter-button-label">counter button</span>
+        <button className="counter-button"
+                aria-labelledby="counter-button-label"
+                onClick={incrementTally}>Click to
           Count
         </button>
       </div>
       <div className="counter-reset-button-container">
         <span className="hidden-label" id="counter-reset-button-label">counter reset button</span>
-        <button className="counter-reset-button" aria-labelledby="counter-reset-button-label" onClick={resetTally}>Click
+        <button className="counter-reset-button"
+                aria-labelledby="counter-reset-button-label"
+                onClick={resetTally}>Click
           to Reset
         </button>
       </div>
